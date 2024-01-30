679dd5aa69f2f177260a9fab2c95310c19a56af8
vitor2233 <vitorguilherme2233@gmail.com>
2024-01-30 14:59:02 -0300

    Fixing files

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,8 +1,8 @@
 import { Get, Query, UseGuards } from "@nestjs/common";
 import { Controller } from "@nestjs/common";
-import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
-import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
-import { PrismaService } from "@/prisma/prisma.service";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { PrismaService } from "@/infra/prisma/prisma.service";
 import { z } from "zod";
 
 const pageQueryParamSchema = z
