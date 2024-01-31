af43ff50b86d4d892a84ed2a46e34497b149c2f3
vitor2233 <vitorguilherme2233@gmail.com>
2024-01-31 14:51:06 -0300

    Adding cryptography use cases and gateways

---
+++ src/domain/forum/application/use-cases/errors/wrong-credentials-error.ts
@@ -0,0 +1,8 @@
+
+import { UseCaseError } from "@/core/errors/use-case-error";
+
+export class WrongCredentialsError extends Error implements UseCaseError {
+    constructor() {
+        super('Credentials are not valid')
+    }
+}
