d33838cd85cb0079daf56876ba0914b5f1c5e05c
vitor2233 <vitorguilherme2233@gmail.com>
2024-01-31 18:26:18 -0300

    Adding EnvService

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { envSchema } from './env';
+import { envSchema } from './env/env';
 import { AuthModule } from './auth/auth.module';
 import { HttpModule } from './http/http.module';
+import { EnvService } from './env/env.service';
+import { EnvModule } from './env/env.module';
 
 @Module({
   imports: [
     AuthModule,
     HttpModule,
+    EnvModule,
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true
     })
   ],
+  providers: [EnvService]
 })
 export class AppModule { }
